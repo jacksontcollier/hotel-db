66b75db1e0ee2ba491b0fe78f9a7af17e10982a1
Your Name <jacksontcollier@gmail.com>
2020-06-13 05:44:55 -0400

    Initial commit

---
+++ src/create_hotel.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS "Hotel" (
+	hotel_no CHAR(4) NOT NULL,
+	hotel_name VARCHAR(30) NOT NULL,
+	city VARCHAR(30) NOT NULL,
+	PRIMARY KEY (hotel_no)
+);
---
+++ src/create_room.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS "Room" (
+	room_no CHAR(4) NOT NULL,
+	hotel_no CHAR(4) NOT NULL,
+	type CHAR(1) NOT NULL CONSTRAINT valid_room_type CHECK (
+		type IN ('S', 'D', 'P')
+	),
+	price DECIMAL(5,2),
+	PRIMARY KEY (room_no, hotel_no),
+	FOREIGN KEY (hotel_no) REFERENCES "Hotel" (hotel_no)
+);
