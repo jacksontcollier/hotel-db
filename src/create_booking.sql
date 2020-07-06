e035eab40f1358019994aafb8279de8738778670
Jackson Collier <jacksontcollier@gmail.com>
2020-07-06 16:23:02 -0400

    Switch to spaces over tabs, use lower case relation names

--- src/create_booking.sql
+++ src/create_booking.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS "Booking" (
-	hotel_no INT NOT NULL,
-	room_no CHAR(4) NOT NULL,
-	guest_no INT NOT NULL,
-	date_from DATE NOT NULL,
-	date_to DATE NOT NULL,
-	PRIMARY KEY (hotel_no, guest_no, date_from),
-	FOREIGN KEY (hotel_no) REFERENCES "Hotel" (hotel_no),
-	FOREIGN KEY (room_no, hotel_no) REFERENCES "Room" (room_no, hotel_no)
+CREATE TABLE IF NOT EXISTS booking (
+    hotel_no INT NOT NULL,
+    room_no CHAR(4) NOT NULL,
+    guest_no INT NOT NULL,
+    date_from DATE NOT NULL,
+    date_to DATE NOT NULL,
+    PRIMARY KEY (hotel_no, guest_no, date_from),
+    FOREIGN KEY (hotel_no) REFERENCES hotel (hotel_no),
+    FOREIGN KEY (room_no, hotel_no) REFERENCES room (room_no, hotel_no)
 );
--- src/create_guest.sql
+++ src/create_guest.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS "Guest" (
-	guest_no INT NOT NULL,
-	first_name VARCHAR(20) NOT NULL,
-	last_name VARCHAR(20) NOT NULL,
-	address VARCHAR(40) NOT NULL,
-	PRIMARY KEY (guest_no)
+CREATE TABLE IF NOT EXISTS guest (
+    guest_no INT NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    address VARCHAR(40) NOT NULL,
+    PRIMARY KEY (guest_no)
 );
