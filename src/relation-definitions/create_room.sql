1d85f6d5a84c2c55251281059190204d9149e567
Jackson Collier <jacksontcollier@gmail.com>
2020-07-10 10:37:35 -0400

    Add sql for creating room relation

--- src/relation-definitions/create_room.sql
+++ src/relation-definitions/create_room.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS room (
     room_no CHAR(4) NOT NULL,
     hotel_no INT NOT NULL,
     type CHAR(1) NOT NULL CONSTRAINT valid_room_type CHECK (
-        type IN ('S', 'D', 'P')
+        type IN ('S', 'D', 'P') /* single, double, penthouse */
     ),
     price DECIMAL(5,2),
     PRIMARY KEY (room_no, hotel_no),
